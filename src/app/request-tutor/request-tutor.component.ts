c5090120b24ba173a00181ecbb0f4ee80d0e0481
SharmilaDeviS <sharmiladevi5795@gmail.com>
2020-10-26 20:00:09 +0530

    commit

---
+++ src/app/request-tutor/request-tutor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-tutor',
+  templateUrl: './request-tutor.component.html',
+  styleUrls: ['./request-tutor.component.scss']
+})
+export class RequestTutorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
